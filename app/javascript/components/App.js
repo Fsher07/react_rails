6be72c24ddf62134fa65faf9bfd5fd95df23dbb7
Fsher07 <furkansalimm@gmail.com>
2022-08-17 11:42:32 +0300

    React-rails

---
+++ app/javascript/components/App.js
@@ -0,0 +1,20 @@
+
+import React from 'react'
+import { Provider } from 'react-redux';
+import {  BrowserRouter, Routes, Route } from 'react-router-dom';
+import Greeting from './Greeting';
+import store from '../redux/ConfigureStore';
+
+const App = () => {
+  return (
+    <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={ <Greeting /> } />
+          </Routes>
+        </BrowserRouter>
+    </Provider>
+  )
+}
+
+export default App;
