a25e9212fcc3287b8c4116801903ac3e6b5f9be4
Fsher07 <furkansalimm@gmail.com>
2022-08-17 22:17:36 +0300

    final

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,4 +1,5 @@
 
+
 import React from 'react'
 import { Provider } from 'react-redux';
 import {  BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -9,12 +10,12 @@ const App = () => {
   return (
     <Provider store={store}>
         <BrowserRouter>
-          <Routes>
-            <Route path="/" element={ <Greeting /> } />
-          </Routes>
+            <Routes>
+                <Route path="/" element={( <Greeting /> )} />
+            </Routes>
         </BrowserRouter>
     </Provider>
   )
-}
+};
 
 export default App;
--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,9 +1,20 @@
-import React from 'react'
+
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchGreetingApi } from '../redux/greetings/greeting';
 
 const Greeting = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchGreetingApi());
+  }, [dispatch]);
+  const message = useSelector((state) => state.greetingsReducer);
+
   return (
-    <div>Greeting</div>
-  )
-}
+          <div>
+            <h2>{ message }</h2>
+          </div>
+        );
+};
 
 export default Greeting;
